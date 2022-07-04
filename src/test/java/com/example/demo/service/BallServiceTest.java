a350c8f000216ad53a03a78346068d12e1bed4be
vaperun <vasyl.andriy.perun@gmail.com>
2022-07-04 11:05:09 +0100

    Added unit test for updateBall happy path.

--- src/test/java/com/example/demo/service/BallServiceTest.java
+++ src/test/java/com/example/demo/service/BallServiceTest.java
@@ -8,12 +8,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,7 +27,7 @@ public class BallServiceTest {
     @Test
     void getBallByIdShouldReturnBallIfIdExists() {
         //given
-        Ball ball1 = new Ball(1L,2,3F);
+        Ball ball1 = new Ball(1L, 2, 3F);
         when(repository.findById(1L)).thenReturn(Optional.of(ball1));
         //when
         Ball realBall = service.getBallById(1L);
@@ -41,4 +40,21 @@ public class BallServiceTest {
         when(repository.findById(1L)).thenReturn(Optional.empty());
         assertThrows(RuntimeException.class, () -> service.getBallById(1L));
     }
+
+    @Test
+    void updateBallShouldUpdateBall() {
+        //given:
+        Ball newBall = new Ball();
+        newBall.setSize(10);
+        newBall.setWeight(22.5F);
+        Ball ball1 = new Ball(1L,   2, 3F);
+        when(repository.findById(1L)).thenReturn(Optional.of(ball1));
+        //when:
+        service.updateBall(1L, newBall);
+        //then:
+        verify(repository).save(ball1);
+        assertEquals(ball1.getSize(), (newBall.getSize()));
+        assertEquals(ball1.getWeight(), (newBall.getWeight()));
+    }
+
 }
